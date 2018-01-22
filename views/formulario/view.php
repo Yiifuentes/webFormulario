ab1cdeb93a00b2ea6ae239d68aee450630810ee2
“Yiifuentes“ <fuentesl15@hotmail.com>
2018-01-22 15:10:34 -0500

    cambio de msn al confirmar elimininacion del elemento

--- views/formulario/view.php
+++ views/formulario/view.php
@@ -19,7 +19,7 @@ use yii\widgets\DetailView;
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'esta seguro de Eliminar este elemento?',
                 'method' => 'post',
             ],
         ]) ?>
